a90369251c216cbfcf724088aaf8f1e032652555
Isaac Reis <isaacreis2807@gmail.com>
2026-02-25 13:33:50 -0300

    First commit

---
+++ src/main/java/com/os/api/ordensdeservicoapi/OrdensDeServicoApiApplication.java
@@ -0,0 +1,13 @@
+package com.os.api.ordensdeservicoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdensDeServicoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrdensDeServicoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/os/api/ordensdeservicoapi/OrdensDeServicoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.os.api.ordensdeservicoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdensDeServicoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
